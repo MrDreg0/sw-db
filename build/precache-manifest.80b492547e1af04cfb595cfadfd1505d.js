a65bf3a136e142a69912a8da9a988e5e3ce37338
MrDreg0 <drak0on@yandex.ru>
2019-08-02 23:25:07 +0400

    Добавлен собранный проект для демонстрации

---
+++ build/precache-manifest.80b492547e1af04cfb595cfadfd1505d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f4aa2abcdcc7b687fafd959fb38f4b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcf53027565a19f928da",
+    "url": "/static/css/main.7248a219.chunk.css"
+  },
+  {
+    "revision": "776c2f533a9ce0ee7fb6",
+    "url": "/static/js/2.3129f4d1.chunk.js"
+  },
+  {
+    "revision": "fcf53027565a19f928da",
+    "url": "/static/js/main.f02ec0fa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
